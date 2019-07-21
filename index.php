b5cac4cd288bd4fa258f886a6135a89d4a662dad
Serge <s.zhohal@gmail.com>
2019-07-21 13:52:27 +0300

    add chart

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+	/* Database connection settings */
+	include "blocks/db_connect.php"; 
+
+	$sql = "SELECT * FROM `usd2byn` ";
+    $result = mysqli_query($mysqlLink, $sql);
+
+	while ($row = mysqli_fetch_array($result)) {
+
+		$course = $course . '"'. $row['course'].'",';
+		$date = $date . '"'. $row['date'] .'",';
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+		<title>Rate Chart</title>
+
+		<style type="text/css">			
+			body{
+				font-family: Arial;
+			    margin: 80px 100px 10px 100px;
+			    padding: 0;
+			    color: white;
+			    text-align: center;
+			    background: #555652;
+			}
+
+			.container {
+				color: #E8E9EB;
+				background: #222;
+				border: #555652 1px solid;
+				padding: 10px;
+			}
+		</style>
+
+	</head>
+
+	<body>	   
+	    <div class="container">	
+	    <h1>Dollar Rate Chart</h1>       
+			<canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+
+			<script>
+				var ctx = document.getElementById("chart").getContext('2d');
+    			var myChart = new Chart(ctx, {
+        		type: 'line',
+		        data: {
+		            labels: [<?php echo $date; ?>],
+		            datasets: 
+		            [{
+		                label: 'USD2BYN',
+		                data: [<?php echo $course; ?>],
+		                backgroundColor: 'transparent',
+		                borderColor:'rgba(255,99,132)',
+		                borderWidth: 3
+		            }]
+		        },
+		     
+		        options: {
+		            scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
+		            tooltips:{mode: 'index'},
+		            legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
+		        }
+		    });
+			</script>
+	    </div>
+	    
+	</body>
+</html>
